ee94f81a23bfdad2729530c111257cfea3f124cf
ImAlex00 <alexhoran2002@gmail.com>
2026-09-25 11:55:27 +0000

    Add contact page

--- hello.py
+++ hello.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>Hello, World, I am a Flask app!</p> <br> <p><a href="/about"About</a><p>'
+        return '<p>Hello, World, I am a Flask app!</p> <br> <p><a href="/about">About</a></p> <p><a href="/contact">Contact</a></p>'
 
 @app.route('/about')
 def display_about():
-	return '<p> This runs on the <a href="https://flask.palletsprojects.com/en/stable/">flask</a> framework</p> <p><a href="/">return</a></p?'
+        return '<p>This runs on the <a href="https://flask.palletsprojects.com/en/stable/">Flask</a> framework</p> <p><a href="/">Return</a></p>'
+
+@app.route('/contact')
+def display_contact():
+        return '<p>Contact me at: alexhoran2002@gmail.com</p> <p><a href="/">Return</a></p>'
